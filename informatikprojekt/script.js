c1df189c7be6bc530f85f4bb3add6055bd93f5b7
Lasse Kaemena <webmaster@lajoka.de>
2025-05-21 17:41:31 +0200

    bugfixes

--- informatikprojekt/script.js
+++ informatikprojekt/script.js
@@ -25,36 +25,59 @@ function buy(event) {
 }
 
 //Galerie
-function shrink(img){
+function shrink(img) {
     img.style.transform = "scale(1)";
     img.style.zIndex = 0;
+    
+    if(img.classList.contains("big_gallery_image")){
+        img.classList.remove("big_gallery_image")
+    } else if(img.classList.contains("big_gallery_image_right")) {
+        img.classList.remove("big_gallery_image_right")
+    } else if(img.classList.contains("big_gallery_image_left")) {
+        img.classList.remove("big_gallery_image_left")
+    }
+    
 }
-function enlarge(img){
+function enlarge(img, position) {
     img.style.transform = "scale(1.5)";
     img.style.zIndex = 1;
     img.style.opacity = 1;
+    img.classList.add("big_gallery_image" + position)
 }
-function hide(img){
+function hide(img) {
     img.style.opacity = 0.6;
 }
-function show(img){
+function show() {
     document.querySelectorAll(".gallery_image").forEach(img => img.style.opacity = 1);
 }
-function max_img(event){
+function max_img(event) {
     const clicked_img = event.target;
-    document.querySelectorAll(".gallery_image").forEach(img => {
-        if(img != clicked_img){
+    let position;
+    document.querySelectorAll(".gallery_image").forEach((img, index) => {
+        if (img != clicked_img) {
             shrink(img);
             hide(img);
+        } else {
+            position = getPosition(index);
         }
     });
     const img_scale = clicked_img.style.transform;
-    if(img_scale == "scale(1.5)"){
+    if (img_scale == "scale(1.5)") {
         shrink(clicked_img);
         show();
     } else {
-        enlarge(clicked_img);
+        enlarge(clicked_img, position);
     }
 };
-
-document.querySelectorAll(".gallery_image").forEach(img => img.addEventListener("click", max_img));
+function getPosition(index){
+    if(index % 3 == 0) {
+        return "_left";
+    } else if(index % 3 == 2){
+        return "_right";
+    } else {
+        return "";
+    }
+}
+document.addEventListener("DOMContentLoaded", () => {
+    document.querySelectorAll(".gallery_image").forEach(img => img.addEventListener("click", max_img));
+});
